670139a7911d56cffab34cbde110b2c467c69269
allaoua-rico <allaoua.boudriou@gmail.com>
2022-02-09 22:25:59 +0100

    gullhouse fixing

--- cloudinary.js
+++ cloudinary.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary');
-const dotenv=require('dotenv');
-dotenv.config();
+// const dotenv=require('dotenv');
+// dotenv.config();
 
 cloudinary.config({ 
     cloud_name: 'dwxiuhqh4', 
